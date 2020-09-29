3a0f2318273a79a3f831eae44c207a16f0fda349
Agui206 <71894074+Agui206@users.noreply.github.com>
2020-09-29 11:38:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title>Manejo de letras y grid</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1>Cesar Aguilar</h1>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<p>Cesar Aguilar <mark>Aguilar</mark>Pitayo</p>
+					<p><del>Janet Guerrero</del></p>
+					<p><a>Janet Guerrero</a></p>
+				</div>
+			</div>
+			<script src="js/jquery-3.5.1.min.js"></script>
+			<script src="js/bootstrap.js"></script>
+		</div>
+	</div>
+
+</body>
+</html>
